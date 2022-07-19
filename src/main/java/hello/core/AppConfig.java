d3d76a342b9a7b806829f87c0ca11575e283eae7
Kijun Nam <snrm123@naver.com>
2022-07-19 15:15:29 +0900

    proxy 사용하여 지연처리

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -16,22 +16,22 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     @Bean
     public MemberService memberService(){
-        System.out.println("call AppConfig.memberService");
+        //System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
-        System.out.println("call AppConfig.memberRepository");
+        //System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public OrderService orderService(){
-        System.out.println("call AppConfig.orderService");
+        //System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     @Bean
     public DiscountPolicy discountPolicy(){
-        System.out.println("call AppConfig.discountPolicy");
+        //System.out.println("call AppConfig.discountPolicy");
         return new RateDiscountPolicy();
     }
 
